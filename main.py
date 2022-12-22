9c7bd16ea9a791161a630c52f0ae7262c483d13c
Mark Estrada <mark.estrada.dev>
2022-12-22 04:09:25 -0600

    update instructions

--- main.py
+++ main.py
@@ -16,17 +16,24 @@ word = words_list[5]
 
 # TODO: Welcome the player
 
+
 # TODO: Establish a means of keeping score (8 guesses is a pretty good number of 'lives')
+
 
 # TODO: Somehow display 'blanks' (try underscores: "______") to represent the number of characters in their word (maybe a loop?)
 
+
 # TODO: Have the player guess a letter with an input, and check if that letter is in the word
 
-# TODO: Replace (ALL) the blanks with that letter if they choose correctly and display the new partial word ("__s__s") to the player
+
+# TODO: Replace (ALL) the corresponding blanks with that letter if they choose correctly and display the new partial word ("__s__s") to the player
 # Remember: you can mutate (alter) a list, but not a string, so you might have to convert it back and forth
+
 
 # TODO: Subtract a guess if they choose wrong and use print to tell the player
 
+
 # TODO: Now the difficult part - run the process again, and have the player choose another letter! (A for loop can work, but try to use a while loop - remember to set an exit condition)
+
 
 # TODO: If the player runs out of guesses they lose. If the blanks are all filled, they win! Close your loop and end the program or maybe try to let the player choose to play again...
